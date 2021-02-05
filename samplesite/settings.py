45deeda4314ca9415b93e0abf4bf986f6bb479ef
sarvilin <a.sarvilin@gmail.com>
2021-02-05 17:17:20 +0300

    Marhrutizator

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bboard.apps.BboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
