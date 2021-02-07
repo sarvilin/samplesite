c2b30d3b49c8b60c9dddc4866e1f440b9f3be1d1
sarvilin <a.sarvilin@gmail.com>
2021-02-07 17:03:50 +0300

    Adding ads

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Bb
 from .models import Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
 
 
 def index(request):
@@ -16,3 +18,16 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+# Контроллер-класс
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
